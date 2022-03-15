8d600ca8be928eb0b36ca0c69f395dfe39b5259e
JuanFermar <juanfermar06@gmail.com>
2022-03-15 20:20:51 +0100

    Convertido a Funcion

--- ReadScript.py
+++ ReadScript.py
@@ -1,55 +1,44 @@
-from operator import contains
-import matplotlib.pyplot as plt
-import numpy as np
 from io import open
 
 #COMENTARIO DE PRUEBA
 
-archivo_text=open("C:\\Users\\JuanF\\Desktop\\TFG\\texto_prueba.txt", "r")
 
-text = archivo_text.read().split("\n")
+def ReadVg(dir):
+    archivo_text=open(dir, "r")
 
-archivo_text.close()
+    text = archivo_text.read().split("\n")
 
-error = False
-x = ([])
-y = ([])
-y_error = ([])
-y_def = ([])
-y_def_error = ([])
+    archivo_text.close()
 
-#Reads the txt file
-for i in text:
-    if not i[0] == "#": 
-        for j in i.split(" "):
-            if not "E" in j:
-                x.append(float(j))
-            else:
-                if not error:
-                    temp = j.split("E")
-                    y.append(float(temp[0]) * (10**(float(temp[1]))))
-                    error=True
+    error = False
+    x = ([])
+    y = ([])
+    y_error = ([])
+    y_def = ([])
+    y_def_error = ([])
+
+    #Reads the txt file
+    for i in text:
+        if not i[0] == "#": #Descarto las lineas que esten comentadas
+            for j in i.split(" "):
+                if not "E" in j:
+                    x.append(float(j))
                 else:
-                    temp = j.split("E")
-                    y_error.append(float(temp[0]) * (10**(float(temp[1]))))
-                    error=False
+                    if not error:
+                        temp = j.split("E")
+                        y.append(float(temp[0]) * (10**(float(temp[1]))))
+                        error=True
+                    else:
+                        temp = j.split("E")
+                        y_error.append(float(temp[0]) * (10**(float(temp[1]))))
+                        error=False
 
 
-for i in range(len(y)):
-    y_def.append(abs((10**6) * y[i]))
-    y_def_error.append(abs((10**6) * y_error[i]))
+    for i in range(len(y)):
+        y_def.append(abs((10**6) * y[i]))
+        y_def_error.append(abs((10**6) * y_error[i]))
+
+    return(x, y_def, y_def_error)
 
 
-#GRAFICA
-plt.yscale("log")
-plt.plot(x,y_def, 'o-')
-plt.xlabel('$V_G (V)$')
-plt.ylabel('$I_D (μA)$')
-plt.show()
-
-# Aqui para ver el estilo de los puntos y las lineas
-# http://research.iac.es/sieinvens/python-course/matplotlib.html
-
-#Leer tecto por filas y columnas
-#https://www.lawebdelprogramador.com/foros/Python/691962-leer-txt-por-fila-y-columnas.html
 
