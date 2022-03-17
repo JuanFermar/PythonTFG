57c33870652af31ff2d6ba124ac5a60851a83855
JuanFermar <juanfermar06@gmail.com>
2022-03-17 23:36:00 +0100

    Scroll bars, Fix function, error graph, colors

--- GraphicGenerator.py
+++ GraphicGenerator.py
@@ -1,64 +1,87 @@
 import ReadScript as rs
 import matplotlib.pyplot as plt
 
-#"C:\\Users\\JuanF\\Desktop\\TFG\\current.dat"
+#"C:\\Users\\JuanF\\Desktop\\TFG\\Experimento1\\current.dat"
 
-a=[]
 log=True
-prueba=["C:\\Users\\JuanF\\Desktop\\TFG\\current.dat", "C:\\Users\\JuanF\\Desktop\\TFG\\texto_prueba.txt"]
+error=False
+tipe = True
 
-# def addButton(dir):
-#     # excepcion no se entra
-#     #si
-#         a.append(dir)
+prueba=["C://Users//JuanF//Desktop//TFG//Experimento1//14nm.dat", "C://Users//JuanF//Desktop//TFG//Experimento1//12nm.dat"]
 
-
-def graphVg(x, y, log, num, axes):    #Graphic generator of Vg vs Id
+def graphId(x, y, e, log, type, error, num):    #Graphic generator of Vg vs Id
 
     if log:
         plt.yscale("log")       #Log Scale
         
     if num == 0:
-        plt.plot(x,y, 'o-')
-    elif num == 1:
-        plt.plot(x,y, 'x-')
-    elif num == 2:
-        plt.plot(x,y, 's-')
-    elif num == 3:
-        plt.plot(x,y, '^-')
-    elif num == 4:
-        plt.plot(x,y, 'D-')
-    else:
-        plt.plot(x,y, '+-')
+        if error:
+            plt.plot(x,y, 'bo-')
+            plt.plot(x,e, 'bo--')
+        else:
+            plt.plot(x,y, 'bo-')
 
-    if axes == "Vg":
+    elif num == 1:
+        if error:
+            plt.plot(x,y, 'rx-')
+            plt.plot(x,e, 'rx--')
+        else:
+            plt.plot(x,y, 'rx-')
+
+    elif num == 2:
+        if error:
+            plt.plot(x,y, 'gs-')
+            plt.plot(x,e, 'gs--')
+        else:
+            plt.plot(x,y, 's-')
+
+    elif num == 3:
+        if error:
+            plt.plot(x,y, 'm^-')
+            plt.plot(x,e, 'm^--')
+        else:
+            plt.plot(x,y, 'm^-')
+
+    elif num == 4:
+        if error:
+            plt.plot(x,y, 'yD-')
+            plt.plot(x,e, 'yD--')
+
+    else:
+        if error:
+            plt.plot(x,y, 'k+-')
+            plt.plot(x,e, 'k+--')
+        else:
+            plt.plot(x,y, 'k+-')
+
+    if type:
         plt.xlabel('$V_G (V)$') #axes
-    elif axes == "Vd":
+    else:
         plt.xlabel('$V_D (V)$')
 
     plt.ylabel('$I_D (μA)$')
 
 
-def graphManage(lista, magnitud):
+def graphManage(lista, log, type, error):
 
     plt.figure()
     
     for i in range(len(lista)):
-        if magnitud == "Vg":
+        if type:
             grafica=rs.ReadVg(lista[i])
-        elif magnitud == "Vd":
+        else:
             grafica=rs.ReadVg(lista[i]) #FUTURE READVD
 
 
         x=grafica[0] 
         y=grafica[1]
         e=grafica[2]
-        graphVg(x, y, log, i, magnitud)
-    
+        graphId(x, y, e, log, type, error, i)
+    #plt.legend(loc="best", facecolor="w", fontsize=16)
     plt.show()
 
 
-graphManage(prueba, "Vd")
+graphManage(prueba, log, tipe, error)
 
 
 # Aqui para ver el estilo de los puntos y las lineas
--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from functools import partial
-
+from tkinter import filedialog
+from turtle import left
 root= Tk()
 
 root.title("Simulación de Transistores")
@@ -10,49 +11,119 @@ root.title("Simulación de Transistores")
 frame1=Frame(root)  #Directions Frame
 frame1.grid(row=0, column=0)
 
-frame2=Frame(root)  #options Freme
+frame2=Frame(root)  #options Frame
 frame2.grid(row=0, column=1)
 
 my_listbox = Listbox(frame1)
 my_listbox.grid(row=1, column=1, padx=5, pady=5)
 
+scrollVert=Scrollbar(frame1,orient="vertical")
+scrollVert.config(command=my_listbox.yview)
+scrollVert.grid(row=1, column=2, sticky="nsew")
+
+
+scrollHori=Scrollbar(frame1, orient="horizontal")
+scrollHori.config(command=my_listbox.xview)
+scrollHori.grid(row=2, column=1, sticky="nsew")
+
+my_listbox.config(yscrollcommand=scrollVert.set, xscrollcommand=scrollHori.set)
 
 #--------------Funciones------------------------
 
-lista=[]
-
 def Add(text):
-    #lista.append(text.get())
     my_listbox.insert(END, text.get())
+    graph1.set("")
     
 
 def Delete():
     my_listbox.delete(ANCHOR)
+
+def Search():
+    frame1.filename = filedialog.askopenfilename(title="search documents", filetypes=(('archivos txt', '*.txt'),('todos los archivos', '*')))
+    newtext = frame1.filename.replace('/', "//")
+    graph1.set(newtext)
+
+    
 
 
 #-----------------Texts Directions----------------
 
 #Text str
 graph1=StringVar()
+graphTitle=StringVar()
 
 #Text Box
 cuadro1=Entry(frame1, textvariable=graph1)
 cuadro1.grid(row=0, column=1, padx=5, pady=5)
 
+cuadro2=Entry(frame2, textvariable=graphTitle)
+cuadro2.grid(row=0, column=1, padx=5, pady=5)
+
 
 #Text Title
 
-text1=Label(frame1, text="Documento 1")
+text1=Label(frame1, text="Documentos:")
+text1.grid(row=0, column=0, padx=5, pady=5)
+
+
+text2=Label(frame2, text="Title:")
 text1.grid(row=0, column=0, padx=5, pady=5)
 
 
 #----------Buttons------------------
 
 bAdd=Button(frame1, text="Add", width=5, command=partial(Add,graph1))
-bAdd.grid(row=0, column=3, padx=5, pady=5)
+bAdd.grid(row=0, column=4, padx=5, pady=5)
 
 bDelete=Button(frame1, text="Delete", width=5, command=Delete)
-bDelete.grid(row=2, column=3, padx=5, pady=5)
+bDelete.grid(row=3, column=4, padx=5, pady=5)
+
+bGraph=Button(frame1, text="Graph",height=1, width=15)
+bGraph.grid(row=3, column=1, padx=5, pady=5)
+
+bSearch = Button(frame1, text="···", command=Search)
+bSearch.grid(row=0, column=3, padx=5, pady=5)
+
+#-----------Graphic options-----------
+
+Log=BooleanVar()
+choiceVg=BooleanVar()
+errorChoice=BooleanVar()
+Log.set(True)
+choiceVg.set(True)
+errorChoice.set(False)
+
+
+scales = Label(frame2, text="Y axe scale:", justify=RIGHT)
+scales.grid(row=1, column=0, padx=5, pady=5)
+
+bLog = Radiobutton(frame2, text="Logarithmic", variable=Log, value=True)
+bLog.grid(row=1, column=1, padx=5, pady=5)
+
+bLin = Radiobutton(frame2, text="Linear", variable=Log, value= False)
+bLin.grid(row=1, column=2, padx=5, pady=5)
+
+
+
+simulationType = Label(frame2, text="Type of simulation:", justify=RIGHT)
+simulationType.grid(row=2, column=0, padx=5, pady=5)
+
+bVg = Radiobutton(frame2, text="Vg", variable=choiceVg, value=True)
+bVg.grid(row=2, column=1, padx=5, pady=5)
+
+bVd = Radiobutton(frame2, text="Vd", variable=choiceVg, value=False)
+bVd.grid(row=2, column=2, padx=5, pady=5)
+
+
+
+error = Label(frame2, text="Graph Error:", justify=RIGHT)
+error.grid(row=3, column=0, padx=5, pady=5)
+
+bError = Radiobutton(frame2, text="Yes", variable=errorChoice, value=True)
+bError.grid(row=3, column=1, padx=5, pady=5)
+
+bError = Radiobutton(frame2, text="No", variable=errorChoice, value=False)
+bError.grid(row=3, column=2, padx=5, pady=5)
 
 root.mainloop()
 
